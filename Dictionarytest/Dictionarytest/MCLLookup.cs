5aee20871cef8749e426ea7e10c2734ddef17120
browningonline <justin@browningonline.org>
2019-04-04 15:58:07 -0400

    Add files via upload

---
+++ Dictionarytest/Dictionarytest/MCLLookup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionarytest
+{
+    public class MCLLookup  
+    {
+        private Dictionary<MCLLookup, double> _lookup = new Dictionary<MCLLookup, double>();
+        private double pa;
+        private double degC;
+       
+
+        public MCLLookup(double pa, double degC)
+        {
+            this.pa = pa;
+            this.degC = degC;
+            
+        }
+
+        public MCLLookup()
+        {
+            _lookup.Add(new MCLLookup(-1000, -60), 84.0);
+            _lookup.Add(new MCLLookup(-1000, -59), 84.0);
+            _lookup.Add(new MCLLookup(-1000, -58), 84.0);
+            _lookup.Add(new MCLLookup(-1000, -57), 84.0);
+        }
+
+        public double GetMCL(int pressureAltitude_ft, int temperature_degC)
+
+        {
+            var roundedPressureAltitude_ft = (pressureAltitude_ft / 1000) * 1000;
+            var key = new MCLLookup(roundedPressureAltitude_ft, temperature_degC);
+            if (_lookup.ContainsKey(key))
+            {
+                return _lookup[key];
+            }
+            return -999.9;
+        }
+    }
+
+}
---
+++ Dictionarytest/Dictionarytest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionarytest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pressure Altitude: ");
+            int pressureAltitude_ft = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Temperature: ");
+            int temperature_degC = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("PA: " + pressureAltitude_ft + "ft MSL");
+            Console.WriteLine("Temp: " + temperature_degC + " C");
+
+            var mclLookup = new MCLLookup();
+            var power_pct = mclLookup.GetMCL(pressureAltitude_ft, temperature_degC);
+
+            
+            Console.WriteLine(power_pct);
+            Console.ReadLine();
+            
+
+        }
+
+    }
+}
